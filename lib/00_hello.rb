8148bd83c5be8a93acd532232c0c62f53a26744f
SULEYMNE <swone098@gmail.com>
2026-02-20 16:30:23 +0000

    Update 00_hello.rb

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -7,7 +7,7 @@ end
 # 2. Méthode qui demande le prénom à l'utilisateur
 def ask_first_name
   puts "Quel est ton prénom ?"
-  print "> "                              #pour la lisibilite
+  print "> "                              # pour la lisibilite
   first_name = gets.chomp                 # gets = lire au clavier, chomp = enlever le retour à la ligne
   return first_name
 end
